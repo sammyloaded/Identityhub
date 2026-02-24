8e02d8fb75d6373f69b16eeec156300e4f177312
Solomon Ayomide Samuel <sammyloadedcon@gmail.com>
2026-02-24 15:59:17 +0100

    Javascript

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-alert('Welcome to Identityhub')
+// alert('Welcome to Identityhub')
+
+let x = 10;
+let y = 50;
+let z = x + y;
+console.log(z)
+
+// number 
+let num = 100;
+let result = num.toString()
+console.log(typeof num)
+console.log(typeof result)
+
+
+// To Fix 
+let num1 = 5.690876;
+const result1 = num1.toFixed(1)
+console.log(result1)
+
+// toPrecision 
+let num2 = 507.7490;
+let result2 = num2.toPrecision(3);
+console.log(result2)
